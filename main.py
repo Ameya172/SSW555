5f2fb1162293ebc383d8db6a220b340c661c5ede
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 20:33:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 # main
 from graph import Graph
-from shortest_path import shortest_path
-from minimum_spanning_tree import minimum_spanning_tree
-from graph_isomorphism import graph_isomorphism
-from utils import display_challenge, get_input
+from shortest_path import ShortestPath
+from minimum_spanning_tree import MinimumSpanningTree
+from graph_isomorphism import GraphIsomorphism
+from utils import Utils
 
 def main():
+    shortest_path_solver = ShortestPath()
+    minimum_spanning_tree_solver = MinimumSpanningTree()
+    graph_isomorphism_solver = GraphIsomorphism()
+    
     while True:
-        challenge = display_challenge()
+        challenge = Utils.display_challenge()
         if not challenge:
             break
-        user_input = get_input()
+
+        graph_data = challenge["graph_data"]
+        user_input = Utils.get_input()
+
+        graph = Graph()
+        for edge in graph_data:
+            graph.add_edge(*edge)
+
         if challenge["type"] == "shortest_path":
-            result = shortest_path(challenge, user_input)
+            start = challenge["start"]
+            end = challenge["end"]
+            expected_path = graph.get_shortest_path(start, end)
+            result = user_input == expected_path
         elif challenge["type"] == "minimum_spanning_tree":
-            result = minimum_spanning_tree(challenge, user_input)
+            expected_mst = graph.get_minimum_spanning_tree()
+            result = sorted(user_input) == sorted(expected_mst)
         elif challenge["type"] == "graph_isomorphism":
-            result = graph_isomorphism(challenge, user_input)
+            other_graph_data = challenge["other_graph"]
+            result = graph.is_isomorphic(other_graph_data) and sorted(user_input) == sorted(other_graph_data)
+
         print("Correct!" if result else "Incorrect!")
 
 if __name__ == "__main__":
     main()
-
