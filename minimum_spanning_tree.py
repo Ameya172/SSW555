b8c38d6f29bc8fc921a0d765159012a05f2042eb
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 17:43:29 +0300

    Update minimum_spanning_tree.py

--- minimum_spanning_tree.py
+++ minimum_spanning_tree.py
@@ -1,1 +1,10 @@
+# minimum_spanning_tree.py
+import networkx as nx
+
+def minimum_spanning_tree(challenge, user_input):
+    G = nx.Graph()
+    G.add_edges_from(challenge["graph_data"])
+
+    mst = nx.minimum_spanning_tree(G).edges()
+    return sorted(user_input) == sorted(mst)
 
