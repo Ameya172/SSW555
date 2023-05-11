4b2dd9114eaf3c047a2b1bf27eecdbc057f24aeb
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 17:42:59 +0300

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -1,1 +1,11 @@
+# shortest_path.py
+import networkx as nx
 
+def shortest_path(challenge, user_input):
+    G = nx.Graph()
+    G.add_edges_from(challenge["graph_data"])
+    start = challenge["start"]
+    end = challenge["end"]
+
+    shortest = nx.shortest_path(G, start, end)
+    return user_input == shortest
