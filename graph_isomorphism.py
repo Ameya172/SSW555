f5e84a4f1f994b4cf05a25565a1098b60666a375
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 20:34:57 +0300

    Update graph_isomorphism.py

--- graph_isomorphism.py
+++ graph_isomorphism.py
@@ -1,11 +1,12 @@
 # graph_isomorphism.py
 import networkx as nx
 
-def graph_isomorphism(challenge, user_input):
-    G1 = nx.Graph()
-    G1.add_edges_from(challenge["graph_data"])
-    G2 = nx.Graph()
-    G2.add_edges_from(challenge["other_graph"])
+class GraphIsomorphism:
+    def compute(self, challenge, user_input):
+        G1 = nx.Graph()
+        G1.add_edges_from(challenge["graph_data"])
+        G2 = nx.Graph()
+        G2.add_edges_from(challenge["other_graph"])
 
-    isomorphism = nx.is_isomorphic(G1, G2)
-    return isomorphism and sorted(user_input) == sorted(challenge["other_graph"])
+        isomorphism = nx.is_isomorphic(G1, G2)
+        return isomorphism and sorted(user_input) == sorted(challenge["other_graph"])
