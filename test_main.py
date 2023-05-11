1fd765525757dc77875f28c926e1874da44e39a4
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 20:47:47 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,44 +1,24 @@
 # test_main.py
-import unittest
-from unittest.mock import patch, MagicMock
-import main
-import utils
-import shortest_path
-import minimum_spanning_tree
-import graph_isomorphism
 
+import unittest
+from unittest.mock import patch
+from main import main
 
 class TestMain(unittest.TestCase):
-    @patch.object(utils, 'display_challenge')
-    @patch.object(utils, 'get_input')
-    @patch.object(shortest_path, 'shortest_path')
-    @patch.object(minimum_spanning_tree, 'minimum_spanning_tree')
-    @patch.object(graph_isomorphism, 'graph_isomorphism')
-    def test_main(self, mock_isomorphism, mock_mst, mock_path, mock_input, mock_challenge):
-        # Setup
-        mock_challenge.side_effect = [
-            {"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4},
-            {"type": "minimum_spanning_tree", "graph_data": [(1, 2), (2, 3), (3, 4), (4, 1)]},
-            {"type": "graph_isomorphism", "graph_data": [(1, 2), (2, 3), (3, 4)], "other_graph": [(5, 6), (6, 7), (7, 8)]},
-            None  # To stop the loop
-        ]
-        mock_input.side_effect = [
-            [(1, 2), (2, 3), (3, 4)],  # Shortest path input
-            [(1, 2), (2, 3), (3, 4), (4, 1)],  # MST input
-            [(5, 6), (6, 7), (7, 8)]  # Graph isomorphism input
-        ]
-        mock_path.return_value = True
-        mock_mst.return_value = True
-        mock_isomorphism.return_value = True
 
-        # Execute
-        main.main()
-
-        # Verify
-        mock_path.assert_called_once_with(mock_challenge.side_effect[0], mock_input.side_effect[0])
-        mock_mst.assert_called_once_with(mock_challenge.side_effect[1], mock_input.side_effect[1])
-        mock_isomorphism.assert_called_once_with(mock_challenge.side_effect[2], mock_input.side_effect[2])
-
+    @patch('utils.Utils.get_input', return_value=[1, 2, 3, 4])
+    @patch('utils.Utils.display_challenge', return_value={"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4})
+    def test_shortest_path_correct(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Correct!')
+    
+    @patch('utils.Utils.get_input', return_value=[1, 2, 4])
+    @patch('utils.Utils.display_challenge', return_value={"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4})
+    def test_shortest_path_incorrect(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Incorrect!')
 
 if __name__ == '__main__':
     unittest.main()
