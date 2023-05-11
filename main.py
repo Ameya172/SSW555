97a9c9b954f387ec52989cb16ebe54a1191142a7
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-11 19:25:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ def main():
         challenge = display_challenge()
         if not challenge:
             break
-        graph_data = challenge["graph_data"]
         user_input = get_input()
         if challenge["type"] == "shortest_path":
-            result = shortest_path(graph_data, user_input)
+            result = shortest_path(challenge, user_input)
         elif challenge["type"] == "minimum_spanning_tree":
-            result = minimum_spanning_tree(graph_data, user_input)
+            result = minimum_spanning_tree(challenge, user_input)
         elif challenge["type"] == "graph_isomorphism":
-            result = graph_isomorphism(graph_data, user_input)
+            result = graph_isomorphism(challenge, user_input)
         print("Correct!" if result else "Incorrect!")
 
 if __name__ == "__main__":
