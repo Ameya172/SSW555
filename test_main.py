d186391f500c047982988d3510073218522eae0e
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-12 05:07:01 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,14 +7,14 @@ from main import main
 class TestMain(unittest.TestCase):
 
     @patch('utils.Utils.get_input', return_value=[1, 2, 3, 4])
-    @patch('utils.Utils.display_challenge', return_value={"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4})
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4}, None])
     def test_shortest_path_correct(self, mock_input, mock_challenge):
         with patch('builtins.print') as mock_print:
             main()
             mock_print.assert_called_once_with('Correct!')
     
     @patch('utils.Utils.get_input', return_value=[1, 2, 4])
-    @patch('utils.Utils.display_challenge', return_value={"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4})
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "shortest_path", "graph_data": [(1, 2), (2, 3), (3, 4)], "start": 1, "end": 4}, None])
     def test_shortest_path_incorrect(self, mock_input, mock_challenge):
         with patch('builtins.print') as mock_print:
             main()
