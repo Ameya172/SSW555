d3e652db10f416258f17b69d5436c0ff2cb0c3a6
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-05-12 05:24:50 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -19,6 +19,34 @@ class TestMain(unittest.TestCase):
         with patch('builtins.print') as mock_print:
             main()
             mock_print.assert_called_once_with('Incorrect!')
+    
+        @patch('utils.Utils.get_input', return_value=[(1, 2), (2, 3), (3, 4), (4, 1)])
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "minimum_spanning_tree", "graph_data": [(1, 2), (2, 3), (3, 4), (4, 1)]}, None])
+    def test_minimum_spanning_tree_correct(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Correct!')
+    
+    @patch('utils.Utils.get_input', return_value=[(1, 2), (2, 3), (3, 1)])
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "minimum_spanning_tree", "graph_data": [(1, 2), (2, 3), (3, 4), (4, 1)]}, None])
+    def test_minimum_spanning_tree_incorrect(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Incorrect!')
+
+    @patch('utils.Utils.get_input', return_value=[(5, 6), (6, 7), (7, 8)])
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "graph_isomorphism", "graph_data": [(1, 2), (2, 3), (3, 1)], "other_graph": [(5, 6), (6, 7), (7, 8)]}, None])
+    def test_graph_isomorphism_correct(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Correct!')
+    
+    @patch('utils.Utils.get_input', return_value=[(5, 6), (6, 7), (7, 5)])
+    @patch('utils.Utils.display_challenge', side_effect=[{"type": "graph_isomorphism", "graph_data": [(1, 2), (2, 3), (3, 1)], "other_graph": [(5, 6), (6, 7), (7, 8)]}, None])
+    def test_graph_isomorphism_incorrect(self, mock_input, mock_challenge):
+        with patch('builtins.print') as mock_print:
+            main()
+            mock_print.assert_called_once_with('Incorrect!')
 
 if __name__ == '__main__':
     unittest.main()
